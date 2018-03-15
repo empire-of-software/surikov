1585a3cdcd2c021751b283c484d51fd1193f650e
Jessica Kerr <jessitron@jessitron.com>
2018-03-14 23:20:45 -0500

    Initial commit from Atomist

---
+++ src/main/java/visiliy/SurikovApplication.java
@@ -0,0 +1,12 @@
+package visiliy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurikovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurikovApplication.class, args);
+	}
+}
